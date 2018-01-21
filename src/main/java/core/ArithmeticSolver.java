f82a29b3fac6843c929f78b342c444b3640e4f5f
anselm-boox <anselm.ogbunugafor@booxmedia.com>
2018-01-22 00:20:41 +0200

    main: added ArithmeticSolver class implementation.

---
+++ src/main/java/core/ArithmeticSolver.java
@@ -0,0 +1,24 @@
+package core;
+
+public class ArithmeticSolver {
+	
+	public double add(double leftOperand, double rightOperand) {
+		return leftOperand + rightOperand;
+	}
+	
+	public double subtract(double leftOperand, double rightOperand) {
+		return leftOperand - rightOperand;
+	}
+	
+	public double multiply(double leftOperand, double rightOperand) {
+		return leftOperand * rightOperand;
+	}
+	
+	public double divide(double leftOperand, double rightOperand) {
+		return leftOperand / rightOperand;
+	}
+	
+	public double modulus(double leftOperand, double rightOperand) {
+		return leftOperand % rightOperand;
+	}
+}
--- src/test/java/core/ArithmeticSolverTest.java
+++ src/test/java/core/ArithmeticSolverTest.java
@@ -7,30 +7,30 @@ public class ArithmeticSolverTest extends TestCase {
 	public void testAdd() {
 		ArithmeticSolver solver = new ArithmeticSolver();
 		
-		assertEquals(4, solver.add(2, 2));
+		assertEquals(4.0, solver.add(2, 2));
 	}
 	
 	public void testSubtract() {
 		ArithmeticSolver solver = new ArithmeticSolver();
 		
-		assertEquals(2, solver.subtract(4, 2));
+		assertEquals(2.0, solver.subtract(4, 2));
 	}
 	
 	public void testMultiply() {
 		ArithmeticSolver solver = new ArithmeticSolver();
 		
-		assertEquals(16, solver.multiply(8, 2));
+		assertEquals(16.0, solver.multiply(8, 2));
 	}
 	
 	public void testDivide() {
 		ArithmeticSolver solver = new ArithmeticSolver();
 		
-		assertEquals(3, solver.divide(9, 3));
+		assertEquals(3.0, solver.divide(9, 3));
 	}
 	
 	public void testModulus() {
 		ArithmeticSolver solver = new ArithmeticSolver();
 		
-		assertEquals(1, solver.modulus(5, 2));
+		assertEquals(1.0, solver.modulus(5, 2));
 	}
 }
