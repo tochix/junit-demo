5a30ad9a629301002b15ff4f598718beb73aa7a3
anselm-boox <anselm.ogbunugafor@booxmedia.com>
2018-01-22 19:35:42 +0200

    test: add more test cases for 'add' method

--- src/test/java/core/ArithmeticSolverTest.java
+++ src/test/java/core/ArithmeticSolverTest.java
@@ -10,6 +10,31 @@ public class ArithmeticSolverTest extends TestCase {
 		assertEquals(4.0, solver.add(2, 2));
 	}
 	
+	public void testAddNegativeNumbers() {
+		ArithmeticSolver solver = new ArithmeticSolver();
+		
+		assertEquals(-4.0, solver.add(-2, -2));
+		assertEquals(-55.0, solver.add(-58, 3));
+		assertEquals(10.0, solver.add(25, -15));
+	}
+	
+	public void testAddLargeNumbers() {
+		ArithmeticSolver solver = new ArithmeticSolver();
+		
+		//assertEquals(Double.MIN_VALUE, solver.add(Double.MAX_VALUE, 1));
+		assertEquals(-58110005.0, solver.add(-58752887, 642882));
+	}
+	
+	public void testAddDecimalNumbers() {
+		ArithmeticSolver solver = new ArithmeticSolver();
+		
+		assertEquals(8.6, solver.add(5.3, 3.3));
+		assertEquals(12.778, solver.add(9.445, 3.333));
+		assertNotNull(solver.add(9.445, 3.333));
+		
+	}
+	
+	
 	public void testSubtract() {
 		ArithmeticSolver solver = new ArithmeticSolver();
 		
